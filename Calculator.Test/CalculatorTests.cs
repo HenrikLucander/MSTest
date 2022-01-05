16e5e164e278e4fb13c8444d36d3c42c9efa73d6
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-01-05 08:17:15 +0200

    Add project files.

---
+++ Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Library;
+using System.Diagnostics;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Test_Divide()
+        {
+            // Arrange
+            int expected = 5;
+            int numerator = 20;
+            int denominatior = 4;
+
+            // Act
+            int actual = Calculator.Divide(numerator, denominatior);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
